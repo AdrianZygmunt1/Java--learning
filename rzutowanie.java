1919ee9655dc053f05d8cd1769b2183f6af109df
Your Name <you@example.com>
2026-03-04 22:13:56 +0100

    day2-2

---
+++ rzutowanie.java
@@ -0,0 +1,13 @@
+public class rzutowanie {
+    public static void main(String[] args) {
+        double a = 42.574;
+        int b = (int) a;// rzutowanie jeden
+        a = (int) a;// rzutowanie dwa
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+/*
+ * Przypisywanie wartości jednego typu do zmiennej innego typu nazywa się
+ * Rzutowanie typów.
+ */
---
+++ rzutowanietypow.java
@@ -0,0 +1,16 @@
+public class rzutowanietypow {
+    
+}
+
+/* 
+dla klas sa dwa typy rzutowan 
+Upcasting
+    -Możesz rzutować instancję podklasy na jej klasę nadrzędną.
+    -Rozważ poniższy przykład, zakładając, że Cat jest podklasą Animal.
+    -Animal a = new Cat();
+    -Java automatycznie dokonała rzutowania w górę zmiennej typu Cat do typu Animal.
+Downcasting
+    -Animal a = new Cat();
+    -((Cat)a).makeSound();
+    -To spróbuje rzutować zmienną a na typ Cat i wywołać jego metodę makeSound().
+    */
